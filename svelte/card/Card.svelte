6b78160b4732d6ec042b016677de93e749a874ea
Udo Bauer <knackwurstking.tux@gmail.com>
2024-03-01 19:44:09 +0100

    fix card background color

--- svelte/card/Card.svelte
+++ svelte/card/Card.svelte
@@ -1,15 +1,17 @@
 <script>
-	export let backgroundColor = 'hsl(var(--card-bg))';
-	export let color = 'hsl(var(--card-fg))';
+	export let backgroundColor = "hsl(var(--card))";
+	export let color = "hsl(var(--card-fg))";
 	export let noBorder = false;
 </script>
 
 <article
 	{...$$restProps}
-	class={'ui-card ' + ($$restProps.class || '')}
+	class={"ui-card " + ($$restProps.class || "")}
 	style:background-color={backgroundColor}
 	style:color
-	style:border={noBorder ? 'none' : 'var(--border-width) var(--border-style) hsl(var(--border))'}
+	style:border={noBorder
+		? "none"
+		: "var(--border-width) var(--border-style) hsl(var(--border))"}
 >
 	<slot />
 </article>
