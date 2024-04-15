ed9478d1e6b72a782ce127624faaf3928526b190
Udo Bauer <knackwurstking.tux@gmail.com>
2024-04-15 03:41:12 +0200

    Formattings... Check listener type

--- src/events/events.js
+++ src/events/events.js
@@ -1,57 +1,60 @@
 export default class Events {
-  /** @type {{[key: string]: ((data: any) => void|Promise<void>)[]}} */
-  listeners;
+    /** @type {{[key: string]: ((data: any) => void|Promise<void>)[]}} */
+    listeners;
 
-  constructor() {
-    this.listeners = {};
-  }
-
-  /**
-   * @param {string} key
-   * @param {any} data
-   */
-  dispatchWithData(key, data) {
-    if (!!this.listeners[key]) {
-      for (const listener of this.listeners[key]) {
-        listener(data);
-      }
+    constructor() {
+        this.listeners = {};
     }
 
-    return this;
-  }
+    /**
+     * @param {string} key
+     * @param {any} data
+     */
+    dispatchWithData(key, data) {
+        if (!!this.listeners[key]) {
+            for (const listener of this.listeners[key]) {
+                listener(data);
+            }
+        }
 
-  /**
-   * @param {string} key
-   * @param {((data: any) => void|Promise<void>) | null} listener
-   * @returns {() => void} clean up function
-   */
-  addListener(key, listener) {
-    if (!this.listeners[key]) {
-      this.listeners[key] = [];
+        return this;
     }
 
-    this.listeners[key].push(listener);
+    /**
+     * @param {string} key
+     * @param {((data: any) => void|Promise<void>) | null} listener
+     * @returns {() => void} clean up function
+     */
+    addListener(key, listener) {
+        if (typeof listener !== "function")
+            throw `invalid event listener passed for "${key}" event!`;
 
-    return () => {
-      this.removeListener(key, listener);
-    };
-  }
+        if (!this.listeners[key]) {
+            this.listeners[key] = [];
+        }
 
-  /**
-   * @param {string} key
-   * @param {((data: any) => void|Promise<void>)} listener
-   */
-  removeListener(key, listener) {
-    if (!this.listeners[key]) return this;
+        this.listeners[key].push(listener);
 
-    let index = 0;
-    for (const l of this.listeners[key]) {
-      if (l === listener) {
-        this.listeners[key].splice(index, 1);
-      }
-      index++;
+        return () => {
+            this.removeListener(key, listener);
+        };
     }
 
-    return this;
-  }
+    /**
+     * @param {string} key
+     * @param {((data: any) => void|Promise<void>)} listener
+     */
+    removeListener(key, listener) {
+        if (!this.listeners[key]) return this;
+
+        let index = 0;
+        for (const l of this.listeners[key]) {
+            if (l === listener) {
+                this.listeners[key].splice(index, 1);
+            }
+            index++;
+        }
+
+        return this;
+    }
 }
