947c66b0b405c54b5e6c255d20a76003ac8ea144
Udo Bauer <knackwurstking.tux@gmail.com>
2024-04-16 10:12:19 +0200

    Add svg: chevron-down

--- src/svg/index.js
+++ src/svg/index.js
@@ -1,4 +1,5 @@
 export { default as BackArrowNavigation } from "./back-arrow-navigation"; // use currentColor
+export { default as ChevronDown } from "./chevron-down"; // use currentColor
 export { default as DeleteRecycleBinTrashCan } from "./delete-recycle-bin-trash-can"; // use currentColor
 export { default as Edit2 } from "./edit-2"; // use currentColor
 export { default as PDFDocument } from "./pdf-document"; // use currentColor
