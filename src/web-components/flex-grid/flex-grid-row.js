d7d1ee33c005c9219ba6585a78c4a6f707fbfcda
Udo Bauer <knackwurstking.tux@gmail.com>
2024-04-17 07:37:13 +0200

    Add flex grid row component

--- src/web-components/flex-grid/flex-grid-row.js
+++ src/web-components/flex-grid/flex-grid-row.js
@@ -1,3 +1,30 @@
+const template = document.createElement("template");
+template.innerHTML = `
+    <style>
+        :host {
+            --row-gap: var(--gap, 0);
+            display: flex;
+            justify-content: flex-start;
+            flex-flow: row nowrap;
+            width: 100%;
+        }
+
+        :host > * {
+            margin: 0 var(--row-gap);
+        }
+
+        :host > *:first-child {
+            margin-left: 0;
+        }
+
+        :host > *:last-child {
+            margin-right: 0;
+        }
+    </style>
+
+    <slot></slot>
+`;
+
 export class FlexGridRow extends HTMLElement {
     constructor() {
         super();
@@ -7,7 +34,10 @@ export class FlexGridRow extends HTMLElement {
      * Runs each time the element is appended to or moved in the DOM
      */
     connectedCallback() {
-        // ...
+        this.style.setProperty("--gap", this.getAttribute("gap") || "0");
+
+        this.attachShadow({ mode: "open" });
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
     }
 
     /**
