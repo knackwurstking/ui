a9858ff07fe8fc4291555ffa68b7a4255ec7df5a
Udo Bauer <knackwurstking.tux@gmail.com>
2024-04-17 04:24:54 +0200

    Update container component

--- src/web-components/container/container.js
+++ src/web-components/container/container.js
@@ -1,3 +1,19 @@
+const template = document.createElement("template");
+template.innerHTML = `
+    <style>
+        div {
+            width: 100%;
+            max-width: 65em;
+            margin: 0 auto;
+            padding: var(--spacing);
+        }
+    </style>
+
+    <div>
+        <slot></slot>
+    </div>
+`;
+
 export class Container extends HTMLElement {
     constructor() {
         super();
@@ -7,7 +23,8 @@ export class Container extends HTMLElement {
      * Runs each time the element is appended to or moved in the DOM
      */
     connectedCallback() {
-        // ...
+        this.attachShadow({ mode: "open" });
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
     }
 
     /**
