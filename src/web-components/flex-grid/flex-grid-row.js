c522944e18677d29d1890809c229b5137a978b60
Udo Bauer <knackwurstking.tux@gmail.com>
2024-04-19 18:49:24 +0200

    Update flex-grid gap

--- src/web-components/flex-grid/flex-grid-row.js
+++ src/web-components/flex-grid/flex-grid-row.js
@@ -3,9 +3,18 @@ template.innerHTML = `
 <style>
     :host {
         --row-gap: var(--gap, 0);
+        margin: var(--gap, 0) 0;
         display: flex;
         flex-flow: row nowrap;
         width: 100%;
+    }
+
+    :host(:first-child) {
+        margin-top: 0;
+    }
+
+    :host(:last-child) {
+        margin-bottom: 0;
     }
 </style>
 
@@ -21,7 +30,7 @@ export class FlexGridRow extends HTMLElement {
      * Runs each time the element is appended to or moved in the DOM
      */
     connectedCallback() {
-        this.style.setProperty("--gap", this.getAttribute("gap") || "0");
+        this.style.setProperty("--row-gap", this.getAttribute("gap") || "0");
 
         this.attachShadow({ mode: "open" });
         this.shadowRoot.appendChild(template.content.cloneNode(true));
