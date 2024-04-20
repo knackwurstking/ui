fc79386e212052781ad1866c1d9fc585da5cfddc
Udo Bauer <knackwurstking.tux@gmail.com>
2024-04-20 20:22:12 +0200

    Remove not use code from ripple.create

--- src/js/ripple/create.js
+++ src/js/ripple/create.js
@@ -3,54 +3,34 @@ import rippleStop from "./stop";
 
 /**
  * @param {Element | HTMLElement} el
- * @param {import(".").RippleOptions} options 
+ * @param {import(".").RippleOptions} options will be passed down the the ripple start function
  */
 export default function create(el, options = {}) {
-  /** @type {HTMLElement} */
-  let ripple;
-  let destroyed = false;
+    /** @type {HTMLElement} */
+    let ripple;
 
-  /** @param {PointerEvent & { currentTarget: HTMLElement }} ev */
-  const _rippleStart = (ev) => {
-    ripple = rippleStart(ev, { ...options });
-  };
+    /** @param {PointerEvent & { currentTarget: HTMLElement }} ev */
+    const _rippleStart = (ev) => {
+        ripple = rippleStart(ev, options);
+    };
 
-  const _rippleStop = () => {
-    rippleStop(ripple);
-  };
+    const _rippleStop = () => {
+        rippleStop(ripple);
+    };
 
-  // Setup ...
-  const setup = () => {
     el.classList.add("ripple-container");
+    // @ts-ignore
+    el.style.overflow = "hidden";
 
     el.addEventListener("pointerdown", _rippleStart);
     el.addEventListener("pointerup", _rippleStop);
     el.addEventListener("pointerleave", _rippleStop);
 
-    destroyed = false;
-  }
+    return () => {
+        el.classList.remove("ripple-container");
 
-  const destroy = () => {
-    el.classList.remove("ripple-container");
-
-    el.removeEventListener("pointerdown", _rippleStart);
-    el.removeEventListener("pointerup", _rippleStop);
-    el.removeEventListener("pointerleave", _rippleStop);
-
-    destroyed = true;
-  }
-
-  if (options) setup()
-
-  return {
-    /**
-     * @param {import(".").RippleOptions} _options
-     */
-    update(_options) {
-      options = _options;
-      if (options && destroyed) setup();
-      else if (!(options || destroyed)) destroy();
-    },
-    destroy,
-  }
+        el.removeEventListener("pointerdown", _rippleStart);
+        el.removeEventListener("pointerup", _rippleStop);
+        el.removeEventListener("pointerleave", _rippleStop);
+    }
 }
