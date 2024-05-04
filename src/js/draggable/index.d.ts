de0774aa00d48d6d92c3f831c38fa6d8e079b276
Udo Bauer <knackwurstking.tux@gmail.com>
2024-05-04 03:59:11 +0200

    renme drag callback keys

--- src/js/draggable/index.d.ts
+++ src/js/draggable/index.d.ts
@@ -1,6 +1,6 @@
 export { default as create } from "./create";
 export type DraggableOptions = {
-    ondragging?: ((index: number) => void | Promise<void>) | null;
-    ondragstart?: ((index: number) => void | Promise<void>) | null;
-    ondragend?: ((startIndex: number, index: number) => void | Promise<void>) | null;
+    onDragging?: ((index: number) => void | Promise<void>) | null;
+    onDragStart?: ((index: number) => void | Promise<void>) | null;
+    onDragEnd?: ((startIndex: number, index: number) => void | Promise<void>) | null;
 };
--- src/js/draggable/index.js
+++ src/js/draggable/index.js
@@ -3,9 +3,9 @@ export { default as create } from "./create"
 /**
  * @typedef DraggableOptions
  * @type {{
- *  ondragging?: ((index: number) => void | Promise<void>) | null;
- *  ondragstart?: ((index: number) => void | Promise<void>) | null;
- *  ondragend?: ((startIndex: number, index: number) => void | Promise<void>) | null;
+ *  onDragging?: ((index: number) => void | Promise<void>) | null;
+ *  onDragStart?: ((index: number) => void | Promise<void>) | null;
+ *  onDragEnd?: ((startIndex: number, index: number) => void | Promise<void>) | null;
  * }}
  */
 
