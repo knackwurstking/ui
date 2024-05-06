384e88db9ad56e33f994d3d2d9b263b376d344fc
Udo Bauer <knackwurstking@Udos-Air.fritz.box>
2024-05-06 05:13:38 +0200

    update npm build

--- dist/main.min.cjs
+++ dist/main.min.cjs
@@ -269,6 +269,8 @@
         margin: var(--ui-spacing) !important;
         margin-top: calc(env(safe-area-inset-top, 0) + var(--ui-spacing)) !important;
         margin-bottom: calc(env(safe-area-inset-bottom, 0) + var(--ui-spacing)) !important;
+
+        padding: 0 !important;
     }
 
     /*
@@ -280,7 +282,7 @@
         align-items: center;
         justify-content: space-between;
 
-        padding: calc(var(--ui-spacing) / 2);
+        padding: 0 var(--ui-spacing);
         padding-left: var(--ui-spacing);
 
         border-top-right-radius: var(--ui-dialog-radius);
@@ -306,8 +308,7 @@
      */
 
     .content {
-        padding-left: var(--ui-spacing);
-        padding-right: var(--ui-spacing);
+        padding: var(--ui-spacing);
         height: fit-content;
     }
 
@@ -325,7 +326,7 @@
      */
 
     footer {
-        padding: var(--ui-spacing);
+        padding: 0 var(--ui-spacing);
         margin-top: var(--ui-spacing) !important;
         border-bottom-right-radius: var(--ui-dialog-radius);
         border-bottom-left-radius: var(--ui-dialog-radius);
