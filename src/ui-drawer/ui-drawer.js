6341825fa824b9ce13876a3dd9249901c9f9fb32
Udo Bauer <knackwurstking@Udos-Air.fritz.box>
2024-06-08 17:58:11 +0200

    Add missing ui-drawer styles

---
+++ src/ui-drawer/ui-drawer.js
@@ -0,0 +1,147 @@
+import { CleanUp } from "../js";
+
+const zIndex = 150;
+const template = document.createElement("template");
+template.innerHTML = `
+    <style>
+        :host {
+            display: block;
+
+            position: absolute;
+            z-index: ${zIndex};
+            top: 0;
+            left: -100%;
+            width: 100%;
+            height: 100%;
+
+            overflow: hidden;
+
+            transition: left 0s ease .5s;
+        }
+
+        :host([open]) {
+            background-color: var(--ui-backdrop-bgColor);
+            backdrop-filter: var(--ui-backdropFilter);
+
+            left: 0;
+
+            transition: none;
+        }
+
+        aside {
+            position: absolute;
+            z-index: ${zIndex};
+            top: 0;
+            left: -100%;
+            width: 18em;
+            max-width: 100%;
+            height: 100%;
+
+            overflow-x: hidden;
+            overflow-y: auto;
+
+            -ms-overflow-style: none;
+            scrollbar-width: none;
+
+            color: var(--ui-color);
+            background-color: var(--ui-backdrop-bgColor);
+            backdrop-filter: var(--ui-backdropFilter);
+
+            transition: left .5s ease;
+        }
+
+        :host([open]) aside {
+            left: 0;
+        }
+    </style>
+
+    <aside>
+        <slot></slot>
+    </aside>
+`;
+
+class UI {
+    /**
+     * @param {UIDrawer} root
+     */
+    constructor(root) {
+        /**
+         * @private
+         */
+        this.root = root
+
+        this.aside = this.root.shadowRoot.querySelector("aside");
+    }
+
+    get open() {
+        return this.root.hasAttribute("open");
+    }
+
+    set open(state) {
+        if (state) {
+            this.root.setAttribute("open", "");
+        } else {
+            this.root.removeAttribute("open");
+        }
+    }
+}
+
+export class UIDrawer extends HTMLElement {
+
+    static register = () => {
+        if (!customElements.get("ui-drawer")) {
+            customElements.define("ui-drawer", UIDrawer);
+        }
+    };
+
+    static observedAttributes = ["open"];
+
+    constructor() {
+        super();
+        this.attachShadow({ mode: "open" });
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+        this.cleanup = new CleanUp();
+        this.ui = new UI(this);
+    }
+
+    connectedCallback() {
+        const onClick = (/** @type {MouseEvent} */ ev) => {
+            ev.stopPropagation();
+            this.ui.open = false;
+        };
+
+        const onClickAside = (/** @type {MouseEvent} */ ev) => {
+            ev.stopPropagation();
+        };
+
+        this.addEventListener("click", onClick);
+        this.ui.aside.addEventListener("click", onClickAside);
+
+        this.cleanup.add(() => {
+            this.ui.aside.removeEventListener("click", onClickAside);
+            this.removeEventListener("click", onClick);
+        });
+    }
+
+    disconnectedCallback() {
+        this.cleanup.run();
+    }
+
+    /**
+     * @param {string} name
+     * @param {string | null} _oldValue
+     * @param {string | null} newValue
+     */
+    attributeChangedCallback(name, _oldValue, newValue) {
+        switch (name) {
+            case "open":
+                if (newValue !== null) {
+                    this.classList.add("open");
+                } else {
+                    this.classList.remove("open");
+                }
+                break;
+        }
+    }
+}
