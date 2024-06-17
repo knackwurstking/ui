2ef8e72c9610a186f3cf49e9dc15cd65821f88dc
Udo Bauer <knackwurstking@Udos-Air.fritz.box>
2024-06-18 01:35:43 +0200

    Clean up dialog component

--- src/ui-dialog/ui-dialog.d.ts
+++ src/ui-dialog/ui-dialog.d.ts
@@ -1,7 +1,7 @@
 /**
  * Special slots to use:
- *  - title: all childrens go into "dialog header > span", just use the `Dialog.ui.title` setter/getter
- *  - actions: all childrens go into "dialog footer > ui-flex-grid-row"
+ *  - **title**: all childrens go into _"dialog header > span"_, just use the `Dialog.ui.title` setter/getter
+ *  - **actions**: all childrens go into _"dialog footer > ui-flex-grid-row"_
  *
  * @template {UIDialogEvents} T
  */
@@ -9,32 +9,36 @@ export class UIDialog<T extends import(".").UIDialogEvents> extends HTMLElement 
     static register: () => void;
     constructor();
     cleanup: CleanUp;
-    /** @type {UI<UIDialogEvents & T>} */
-    ui: UI<UIDialogEvents & T>;
+    ui: {
+        /** @type {Events<T>} */
+        events: Events<T>;
+        /**
+         * @private
+         * @type {HTMLElement}
+         */
+        h4: HTMLElement;
+        /**
+         * @private
+         * @type {HTMLDialogElement}
+         */
+        dialog: HTMLDialogElement;
+        getFullscreen: () => boolean;
+        /**
+         * @param {boolean} state
+         */
+        setFullscreen: (state: boolean) => void;
+        getTitle: () => string;
+        /**
+         * @param {string} value
+         */
+        setTitle: (value: string) => void;
+        getDialogElement: () => HTMLDialogElement;
+        open: (modal?: boolean, inert?: boolean) => void;
+        close: () => void;
+    };
     connectedCallback(): void;
     disconnectedCallback(): void;
 }
 export type UIDialogEvents = import(".").UIDialogEvents;
 import { CleanUp } from "../js";
-/**
- * @template {UIDialogEvents} T
- */
-declare class UI<T extends import(".").UIDialogEvents> {
-    /**
-     * @param {UIDialog} root
-     * @param {HTMLDialogElement} dialog
-     */
-    constructor(root: UIDialog<any>, dialog: HTMLDialogElement);
-    /** @type {Events<T>} */
-    events: Events<T>;
-    set fullscreen(state: boolean);
-    get fullscreen(): boolean;
-    set title(value: string);
-    get title(): string;
-    getDialogElement(): HTMLDialogElement;
-    open(modal?: boolean, inert?: boolean): void;
-    close(): void;
-    #private;
-}
 import { Events } from "../js";
-export {};
--- src/ui-dialog/ui-dialog.js
+++ src/ui-dialog/ui-dialog.js
@@ -7,257 +7,187 @@ import { SvgClose } from "../svg";
  * @typedef {import(".").UIDialogEvents} UIDialogEvents
  */
 
-// {{{ HTML Content
-const innerHTML = html`
-<style>
-    * { box-sizing: border-box; }
+const content = html`
+    <style>
+        * { box-sizing: border-box; }
 
-    :host dialog * {
-        box-sizing: border-box;
-    }
-
-    dialog {
-        position: fixed !important;
-
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-
-        max-width: 100%;
-        max-height: 100%;
-
-        margin: 0;
-        padding: 0;
-
-        border: none;
-        outline: none;
-
-        background-color: transparent;
-
-        -ms-overflow-style: none;
-        scrollbar-width: none;
-
-        z-index: 999;
-    }
-
-    dialog::-webkit-scrollbar {
-        display: none;
-    }
-
-    dialog::backdrop {
-        background-color: var(--ui-backdrop-bgColor);
-        backdrop-filter: var(--ui-backdropFilter);
-    }
-
-    dialog > .container {
-        background-color: var(--ui-bgColor);
-        color: var(--ui-color);
-
-        border: 1px solid var(--ui-borderColor);
-        border-radius: var(--ui-radius);
-
-        padding: var(--ui-spacing);
-
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-
-        position: relative;
-    }
-
-    :host([fullscreen]) dialog {
-        width: 100%;
-        height: 100%;
-    }
-
-    :host([fullscreen]) dialog > .container {
-        width: calc(100% - var(--ui-spacing) * 2);
-        height: calc(100% - (env(safe-area-inset-top, 0) + env(safe-area-inset-bottom, 0) + (var(--ui-spacing) * 2)));
-
-        margin: var(--ui-spacing);
-        margin-top: calc(env(safe-area-inset-top, 0) + var(--ui-spacing));
-        margin-bottom: calc(env(safe-area-inset-bottom, 0) + var(--ui-spacing));
-    }
-
-    /*
-     * Header Styles
-     */
-  
-    .header {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-
-        border-top-right-radius: var(--ui-radius);
-        border-top-left-radius: var(--ui-radius);
-
-        width: 100%;
-        height: var(--ui-dialog-header-height);
-    }
-
-    .header h4 {
-        margin: auto 0;
-    }
-
-    :host([fullscreen]) .header {
-        z-index: 15;
-        position: absolute;
-        top: var(--ui-spacing);
-        right: var(--ui-spacing);
-        left: var(--ui-spacing);
-        width: calc(100% - var(--ui-spacing) * 2);
-    }
-
-    /*
-     * Content Styles
-     */
-
-    .content {
-        padding: var(--ui-spacing);
-        height: fit-content;
-        min-width: fit-content;
-        width: 100%;
-    }
-
-    :host([fullscreen]) .content {
-        z-index: 10;
-        position: absolute;
-        top: calc(var(--ui-dialog-header-height) + var(--ui-spacing));
-        bottom: calc(var(--ui-dialog-footer-height) + var(--ui-spacing));
-        right: var(--ui-spacing);
-        left: var(--ui-spacing);
-        padding: unset;
-        height: unset;
-        width: unset;
-        min-width: unset;
-    }
-
-    /*
-     * Footer Styles
-     */
-
-    .footer {
-        margin-top: var(--ui-spacing);
-        border-bottom-right-radius: var(--ui-radius);
-        border-bottom-left-radius: var(--ui-radius);
-
-        width: 100%;
-        height: var(--ui-dialog-footer-height);
-    }
-
-    :host([fullscreen]) .footer {
-        z-index: 15;
-        position: absolute;
-        right: var(--ui-spacing);
-        bottom: var(--ui-spacing);
-        left: var(--ui-spacing);
-        width: calc(100% - var(--ui-spacing) * 2);
-    }
-
-    .footer ui-flex-grid-row {
-        height: 100%;
-        flex-wrap: nowrap;
-        justify-content: flex-end;
-        align-items: center;
-    }
-</style>
-
-<dialog>
-	<div class="container">
-        <div class="header">
-            <span style="white-space: nowrap;"><slot name="title"></slot></span>
-
-            <ui-icon-button style="width: var(--ui-dialog-header-height); height: 100%;" ghost>
-                <svg-close></svg-close>
-            </ui-icon-button>
-        </div>
-
-        <div class="content">
-            <slot></slot>
-        </div>
-
-        <div class="footer">
-            <ui-flex-grid-row gap="calc(var(--ui-spacing) / 2)">
-                <slot name="actions"></slot>
-            </ui-flex-grid-row>
-        </div>
-	</div>
-</dialog>
-`;
-// }}}
-
-/**
- * @template {UIDialogEvents} T
- */
-class UI {
-    /** @type {UIDialog} */
-    #root
-    /** @type {HTMLDialogElement} */
-    #dialog
-    /** @type {HTMLElement} */
-    #h4
-
-    /**
-     * @param {UIDialog} root
-     * @param {HTMLDialogElement} dialog
-     */
-    constructor(root, dialog) {
-        this.#root = root
-        this.#dialog = dialog
-
-        /** @type {Events<T>} */
-        this.events = new Events()
-
-        this.#h4 = document.createElement("h4")
-        this.#h4.slot = "title"
-        this.#root.appendChild(this.#h4)
-    }
-
-    get fullscreen() {
-        return this.#root.hasAttribute("fullscreen")
-    }
-
-    set fullscreen(state) {
-        if (!!state) this.#root.setAttribute("fullscreen", "")
-        else this.#root.removeAttribute("fullscreen")
-    }
-
-    get title() {
-        return this.#h4.innerText
-    }
-
-    set title(value) {
-        this.#h4.innerText = value
-    }
-
-    getDialogElement() {
-        return this.#dialog
-    }
-
-    open(modal = false, inert = true) {
-        const inertBackup = this.#dialog.inert;
-        this.#dialog.inert = inert;
-
-        if (!!modal) {
-            this.#dialog.showModal();
-        } else {
-            this.#dialog.show();
+        :host dialog * {
+            box-sizing: border-box;
         }
 
-        this.events.dispatch("open", null);
-        this.#dialog.inert = inertBackup;
-    }
+        dialog {
+            position: fixed !important;
 
-    close() {
-        this.#dialog.close();
-        this.events.dispatch("close", null);
-    }
-}
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+
+            max-width: 100%;
+            max-height: 100%;
+
+            margin: 0;
+            padding: 0;
+
+            border: none;
+            outline: none;
+
+            background-color: transparent;
+
+            -ms-overflow-style: none;
+            scrollbar-width: none;
+
+            z-index: 999;
+        }
+
+        dialog::-webkit-scrollbar {
+            display: none;
+        }
+
+        dialog::backdrop {
+            background-color: var(--ui-backdrop-bgColor);
+            backdrop-filter: var(--ui-backdropFilter);
+        }
+
+        dialog > .container {
+            background-color: var(--ui-bgColor);
+            color: var(--ui-color);
+
+            border: 1px solid var(--ui-borderColor);
+            border-radius: var(--ui-radius);
+
+            padding: var(--ui-spacing);
+
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+
+            position: relative;
+        }
+
+        :host([fullscreen]) dialog {
+            width: 100%;
+            height: 100%;
+        }
+
+        :host([fullscreen]) dialog > .container {
+            width: calc(100% - var(--ui-spacing) * 2);
+            height: calc(100% - (env(safe-area-inset-top, 0) + env(safe-area-inset-bottom, 0) + (var(--ui-spacing) * 2)));
+
+            margin: var(--ui-spacing);
+            margin-top: calc(env(safe-area-inset-top, 0) + var(--ui-spacing));
+            margin-bottom: calc(env(safe-area-inset-bottom, 0) + var(--ui-spacing));
+        }
+
+        /*
+        * Header Styles
+        */
+    
+        .header {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+
+            border-top-right-radius: var(--ui-radius);
+            border-top-left-radius: var(--ui-radius);
+
+            width: 100%;
+            height: var(--ui-dialog-header-height);
+        }
+
+        .header h4 {
+            margin: auto 0;
+        }
+
+        :host([fullscreen]) .header {
+            z-index: 15;
+            position: absolute;
+            top: var(--ui-spacing);
+            right: var(--ui-spacing);
+            left: var(--ui-spacing);
+            width: calc(100% - var(--ui-spacing) * 2);
+        }
+
+        /*
+        * Content Styles
+        */
+
+        .content {
+            padding: var(--ui-spacing);
+            height: fit-content;
+            min-width: fit-content;
+            width: 100%;
+        }
+
+        :host([fullscreen]) .content {
+            z-index: 10;
+            position: absolute;
+            top: calc(var(--ui-dialog-header-height) + var(--ui-spacing));
+            bottom: calc(var(--ui-dialog-footer-height) + var(--ui-spacing));
+            right: var(--ui-spacing);
+            left: var(--ui-spacing);
+            padding: unset;
+            height: unset;
+            width: unset;
+            min-width: unset;
+        }
+
+        /*
+        * Footer Styles
+        */
+
+        .footer {
+            margin-top: var(--ui-spacing);
+            border-bottom-right-radius: var(--ui-radius);
+            border-bottom-left-radius: var(--ui-radius);
+
+            width: 100%;
+            height: var(--ui-dialog-footer-height);
+        }
+
+        :host([fullscreen]) .footer {
+            z-index: 15;
+            position: absolute;
+            right: var(--ui-spacing);
+            bottom: var(--ui-spacing);
+            left: var(--ui-spacing);
+            width: calc(100% - var(--ui-spacing) * 2);
+        }
+
+        .footer ui-flex-grid-row {
+            height: 100%;
+            flex-wrap: nowrap;
+            justify-content: flex-end;
+            align-items: center;
+        }
+    </style>
+
+    <dialog>
+        <div class="container">
+            <div class="header">
+                <span style="white-space: nowrap;"><slot name="title"></slot></span>
+
+                <ui-icon-button style="width: var(--ui-dialog-header-height); height: 100%;" ghost>
+                    <svg-close></svg-close>
+                </ui-icon-button>
+            </div>
+
+            <div class="content">
+                <slot></slot>
+            </div>
+
+            <div class="footer">
+                <ui-flex-grid-row gap="calc(var(--ui-spacing) / 2)">
+                    <slot name="actions"></slot>
+                </ui-flex-grid-row>
+            </div>
+        </div>
+    </dialog>
+`;
 
 /**
  * Special slots to use:
- *  - title: all childrens go into "dialog header > span", just use the `Dialog.ui.title` setter/getter
- *  - actions: all childrens go into "dialog footer > ui-flex-grid-row"
+ *  - **title**: all childrens go into _"dialog header > span"_, just use the `Dialog.ui.title` setter/getter
+ *  - **actions**: all childrens go into _"dialog footer > ui-flex-grid-row"_
  *
  * @template {UIDialogEvents} T
  */
@@ -275,12 +205,78 @@ export class UIDialog extends HTMLElement {
 
     constructor() {
         super()
-        this.attachShadow({ mode: "open" })
-        this.shadowRoot.innerHTML = innerHTML
+        this.attachShadow({ mode: "open" });
+        this.shadowRoot.innerHTML = content;
 
         this.cleanup = new CleanUp();
-        /** @type {UI<UIDialogEvents & T>} */
-        this.ui = new UI(this, this.shadowRoot.querySelector("dialog"));
+
+        this.ui = {
+            /** @type {Events<T>} */
+            events: new Events(),
+
+            /**
+             * @private
+             * @type {HTMLElement}
+             */
+            h4: (() => {
+                const h4 = document.createElement("h4");
+                h4.slot = "title";
+                this.appendChild(h4);
+                return h4;
+            })(),
+
+            /**
+             * @private
+             * @type {HTMLDialogElement}
+             */
+            dialog: this.shadowRoot.querySelector("dialog"),
+
+            getFullscreen: () => {
+                return this.hasAttribute("fullscreen")
+            },
+
+            /**
+             * @param {boolean} state
+             */
+            setFullscreen: (state) => {
+                if (!!state) this.setAttribute("fullscreen", "")
+                else this.removeAttribute("fullscreen")
+            },
+
+            getTitle: () => {
+                return this.ui.h4.innerText
+            },
+
+            /**
+             * @param {string} value
+             */
+            setTitle: (value) => {
+                this.ui.h4.innerText = value
+            },
+
+            getDialogElement: () => {
+                return this.ui.dialog
+            },
+
+            open: (modal = false, inert = true) => {
+                const inertBackup = this.ui.dialog.inert;
+                this.ui.dialog.inert = inert;
+
+                if (!!modal) {
+                    this.ui.dialog.showModal();
+                } else {
+                    this.ui.dialog.show();
+                }
+
+                this.ui.events.dispatch("open", null);
+                this.ui.dialog.inert = inertBackup;
+            },
+
+            close: () => {
+                this.ui.dialog.close();
+                this.ui.events.dispatch("close", null);
+            },
+        };
     }
 
     connectedCallback() {
