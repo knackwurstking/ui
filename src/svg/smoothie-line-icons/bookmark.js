dc2922da3372e400ca7fa244d89a955a7c534d79
Udo Bauer <knackwurstking.tux@gmail.com>
2024-08-02 19:47:26 +0200

    Set fill to none

--- src/svg/smoothie-line-icons/bookmark.js
+++ src/svg/smoothie-line-icons/bookmark.js
@@ -9,7 +9,7 @@ export default html`
       stroke-linejoin="round"
     ></g>
     <g id="SVGRepo_iconCarrier">
-      <rect width="24" height="24" fill="white"></rect>
+      <rect width="24" height="24" fill="none"></rect>
       <path
         d="M5 19.6693V4C5 3.44772 5.44772 3 6 3H18C18.5523 3 19 3.44772 19 4V19.6693C19 20.131 18.4277 20.346 18.1237 19.9985L12 13L5.87629 19.9985C5.57227 20.346 5 20.131 5 19.6693Z"
         stroke="currentColor"
