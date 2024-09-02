55ebb87b0618ac63feb55879d926120f9c5b1089
Udo Bauer <knackwurstking.tux@gmail.com>
2024-09-02 22:31:32 +0200

    Formatting...

--- src/ui-button/ui-icon-button.js
+++ src/ui-button/ui-icon-button.js
@@ -22,183 +22,183 @@ import { Events, html, ripple } from "../utils";
  *  - *
  */
 export class UIIconButton extends HTMLElement {
-  static register = () => {
-    if (!customElements.get("ui-icon-button")) {
-      customElements.define("ui-icon-button", UIIconButton);
-    }
-  };
-
-  static observedAttributes = ["noripple"];
-
-  constructor() {
-    super();
-    this.attachShadow({ mode: "open" });
-
-    /** @type {import("../utils").Ripple | null} */
-    this.ripple = null;
-
-    this.ui = {
-      root: this,
-
-      /**
-       * @type {Events<UIIconButton_Events>}
-       */
-      events: new Events(),
-
-      get noripple() {
-        return !this.root.removeRippleCallback;
-      },
-
-      set noripple(state) {
-        if (!state) {
-          if (!!this.root.removeRippleCallback) return;
-
-          this.root.removeRippleCallback = ripple.create(this.root);
+    static register = () => {
+        if (!customElements.get("ui-icon-button")) {
+            customElements.define("ui-icon-button", UIIconButton);
         }
-
-        if (!this.root.removeRippleCallback) return;
-
-        this.root.ripple.destroy();
-        this.root.removeRippleCallback = null;
-      },
-
-      get color() {
-        return this.root.getAttribute("color");
-      },
-
-      set color(value) {
-        if (!value) {
-          this.root.removeAttribute("color");
-          return;
-        }
-
-        this.root.setAttribute("color", value);
-      },
-
-      get ghost() {
-        return this.root.hasAttribute("ghost");
-      },
-
-      set ghost(state) {
-        if (!state) {
-          this.root.removeAttribute("ghost");
-          return;
-        }
-
-        this.root.setAttribute("ghost", "");
-      },
-
-      get disabled() {
-        return this.root.hasAttribute("disabled");
-      },
-
-      set disabled(state) {
-        if (!state) {
-          this.root.removeAttribute("disabled");
-          return;
-        }
-
-        this.root.setAttribute("disabled", "");
-      },
     };
 
-    this.shadowRender();
-  }
+    static observedAttributes = ["noripple"];
 
-  shadowRender() {
-    this.shadowRoot.innerHTML = html`
-      <style>
-        * {
-          box-sizing: border-box;
-        }
+    constructor() {
+        super();
+        this.attachShadow({ mode: "open" });
 
-        :host {
-          display: block;
-          position: relative;
-          width: 2.5rem;
-          height: 2.5rem;
-          padding: calc(var(--ui-spacing) / 2);
-          border: 1px solid currentColor;
-          border-radius: var(--ui-radius);
-          outline: none;
-          overflow: hidden;
-          cursor: pointer;
-          user-select: none;
-          font-size: 1.1rem;
-          font-weight: 450;
-          font-family: var(--ui-fontFamily);
-          font-variation-settings: var(--ui-button-fontVariation);
-          transition: color 0.5s linear;
-        }
+        /** @type {import("../utils").Ripple | null} */
+        this.ripple = null;
 
-        :host([ghost]) {
-          border-color: transparent !important;
-          box-shadow: none;
-          font-weight: 900;
-        }
+        this.ui = {
+            root: this,
 
-        :host([color="primary"]) {
-          color: var(--ui-primary-bgColor);
-          border-color: var(--ui-primary-bgColor);
-        }
+            /**
+             * @type {Events<UIIconButton_Events>}
+             */
+            events: new Events(),
 
-        :host([color="secondary"]) {
-          color: var(--ui-secondary-bgColor);
-          border-color: var(--ui-secondary-bgColor);
-        }
+            get noripple() {
+                return !this.root.removeRippleCallback;
+            },
 
-        :host([color="destructive"]) {
-          color: var(--ui-destructive-bgColor);
-          border-color: var(--ui-destructive-bgColor);
-        }
+            set noripple(state) {
+                if (!state) {
+                    if (!!this.root.removeRippleCallback) return;
 
-        /* :disabled */
+                    this.root.removeRippleCallback = ripple.create(this.root);
+                }
 
-        :host([disabled]),
-        :host([disabled]:hover),
-        :host([disabled]:active) {
-          opacity: 0.25;
-          cursor: default;
-          pointer-events: none;
-        }
+                if (!this.root.removeRippleCallback) return;
 
-        ui-svg {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-        }
-      </style>
+                this.root.ripple.destroy();
+                this.root.removeRippleCallback = null;
+            },
 
-      <ui-svg>
-        <slot></slot>
-      </ui-svg>
-    `;
+            get color() {
+                return this.root.getAttribute("color");
+            },
 
-    if (typeof this.removeRippleCallback !== "function") {
-      this.removeRippleCallback = ripple.create(this, { centered: true });
+            set color(value) {
+                if (!value) {
+                    this.root.removeAttribute("color");
+                    return;
+                }
+
+                this.root.setAttribute("color", value);
+            },
+
+            get ghost() {
+                return this.root.hasAttribute("ghost");
+            },
+
+            set ghost(state) {
+                if (!state) {
+                    this.root.removeAttribute("ghost");
+                    return;
+                }
+
+                this.root.setAttribute("ghost", "");
+            },
+
+            get disabled() {
+                return this.root.hasAttribute("disabled");
+            },
+
+            set disabled(state) {
+                if (!state) {
+                    this.root.removeAttribute("disabled");
+                    return;
+                }
+
+                this.root.setAttribute("disabled", "");
+            },
+        };
+
+        this.shadowRender();
     }
 
-    this.addEventListener("click", () => {
-      this.ui.events.dispatch("click", this);
-    });
-  }
+    shadowRender() {
+        this.shadowRoot.innerHTML = html`
+            <style>
+                * {
+                    box-sizing: border-box;
+                }
 
-  connectedCallback() {
-    this.setAttribute("role", "button");
-  }
+                :host {
+                    display: block;
+                    position: relative;
+                    width: 2.5rem;
+                    height: 2.5rem;
+                    padding: calc(var(--ui-spacing) / 2);
+                    border: 1px solid currentColor;
+                    border-radius: var(--ui-radius);
+                    outline: none;
+                    overflow: hidden;
+                    cursor: pointer;
+                    user-select: none;
+                    font-size: 1.1rem;
+                    font-weight: 450;
+                    font-family: var(--ui-fontFamily);
+                    font-variation-settings: var(--ui-button-fontVariation);
+                    transition: color 0.5s linear;
+                }
 
-  disconnectedCallback() {}
+                :host([ghost]) {
+                    border-color: transparent !important;
+                    box-shadow: none;
+                    font-weight: 900;
+                }
 
-  /**
-   * @param {string} name
-   * @param {string | null} _oldValue
-   * @param {string | null} newValue
-   */
-  attributeChangedCallback(name, _oldValue, newValue) {
-    switch (name) {
-      case "noripple":
-        this.ui.noripple = newValue !== null;
-        break;
+                :host([color="primary"]) {
+                    color: var(--ui-primary-bgColor);
+                    border-color: var(--ui-primary-bgColor);
+                }
+
+                :host([color="secondary"]) {
+                    color: var(--ui-secondary-bgColor);
+                    border-color: var(--ui-secondary-bgColor);
+                }
+
+                :host([color="destructive"]) {
+                    color: var(--ui-destructive-bgColor);
+                    border-color: var(--ui-destructive-bgColor);
+                }
+
+                /* :disabled */
+
+                :host([disabled]),
+                :host([disabled]:hover),
+                :host([disabled]:active) {
+                    opacity: 0.25;
+                    cursor: default;
+                    pointer-events: none;
+                }
+
+                ui-svg {
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                }
+            </style>
+
+            <ui-svg>
+                <slot></slot>
+            </ui-svg>
+        `;
+
+        if (typeof this.removeRippleCallback !== "function") {
+            this.removeRippleCallback = ripple.create(this, { centered: true });
+        }
+
+        this.addEventListener("click", () => {
+            this.ui.events.dispatch("click", this);
+        });
     }
-  }
+
+    connectedCallback() {
+        this.setAttribute("role", "button");
+    }
+
+    disconnectedCallback() {}
+
+    /**
+     * @param {string} name
+     * @param {string | null} _oldValue
+     * @param {string | null} newValue
+     */
+    attributeChangedCallback(name, _oldValue, newValue) {
+        switch (name) {
+            case "noripple":
+                this.ui.noripple = newValue !== null;
+                break;
+        }
+    }
 }
