ab751bf01f562eb8ad10069af5a9fa9fc2fce145
Udo Bauer <knackwurstking.tux@gmail.com>
2024-09-20 09:49:31 +0200

    Add ui-alert variants "info" and "error"

--- src/ui-alert/ui-alert.js
+++ src/ui-alert/ui-alert.js
@@ -1,9 +1,12 @@
 import { UIFlexGridItem } from "../ui-flex-grid";
-import { UIPrimary } from "../ui-text";
-import { html } from "../utils";
+import { css, html } from "../utils";
 
 /**
  * HTML: `ui-alert`
+ *
+ * Attributes:
+ *  - **message**: `string`
+ *  - **variant**: `info` | `error`
  */
 export class UIAlert extends UIFlexGridItem {
     static register = () => {
@@ -13,7 +16,7 @@ export class UIAlert extends UIFlexGridItem {
     };
 
     static get observedAttributes() {
-        return ["message", ...super.observedAttributes];
+        return ["message", "variant", ...super.observedAttributes];
     }
 
     /**
@@ -27,6 +30,22 @@ export class UIAlert extends UIFlexGridItem {
          * @type {HTMLElement}
          */
         this.messageContainer = null;
+
+        this.styleVariants = {
+            info: css`
+                :host {
+                    color: var(--ui-primary-fg);
+                    background-color: var(--ui-primary);
+                }
+            `,
+
+            error: css`
+                :host {
+                    color: var(--ui-destructive-fg);
+                    background-color: var(--ui-destructive);
+                }
+            `,
+        };
 
         this.ui = {
             root: this,
@@ -45,10 +64,29 @@ export class UIAlert extends UIFlexGridItem {
                     .innerHTML;
             },
 
+            /**
+             * @param {string} value
+             */
             set message(value) {
                 const container =
                     this.root.shadowRoot.querySelector("ui-primary");
                 container.innerHTML = value || "";
+            },
+
+            get variant() {
+                return this.root.getAttribute("variant");
+            },
+
+            /**
+             * @param {"info" | "error"} value
+             */
+            set variant(value) {
+                if (!value) {
+                    this.root.removeAttribute("variant");
+                    return;
+                }
+
+                this.root.setAttribute("variant", value);
             },
         };
 
@@ -63,15 +101,13 @@ export class UIAlert extends UIFlexGridItem {
             <style>
                 :host {
                     padding: var(--ui-spacing);
-
-                    /** TODO: just debugging here... */
-                    background-color: var(--ui-card);
-                    color: var(--ui-card-fg);
-                    border: 1px solid var(--ui-card-borderColor);
-                    border-radius: var(--ui-card-radius);
+                    border-radius: var(--ui-radius);
+                    border: 1px solid var(--ui-borderColor);
                     padding: var(--ui-spacing);
                 }
             </style>
+
+            <style class="variant"></style>
 
             <ui-primary></ui-primary>
         `;
@@ -95,6 +131,11 @@ export class UIAlert extends UIFlexGridItem {
             case "message":
                 this.ui.message = newValue;
                 break;
+
+            case "variant":
+                this.shadowRoot.querySelector("style.variant").innerHTML =
+                    // @ts-ignore
+                    this.styleVariants[newValue] || "";
         }
     }
 }
