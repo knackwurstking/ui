0414e22d6ce803a21b33f5df697badf9fe3a1088
Udo Bauer <knackwurstking.tux@gmail.com>
2024-10-09 12:14:56 +0200

    Remove dropdown

--- src/ui-input/index.js
+++ src/ui-input/index.js
@@ -4,5 +4,3 @@ export * from "./ui-search";
 export * from "./ui-select";
 export * from "./ui-select-option";
 export * from "./ui-textarea";
-export * from "./ui-dropdown";
-export * from "./ui-dropdown-option";
