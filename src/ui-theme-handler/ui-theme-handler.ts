35159ec518453c4f6ed695ed6608a55f9bbca7e3
Udo Bauer <knackwurstking.tux@gmail.com>
2024-10-09 17:40:53 +0200

    Update attribute handling "theme"

--- src/ui-theme-handler/ui-theme-handler.ts
+++ src/ui-theme-handler/ui-theme-handler.ts
@@ -28,126 +28,23 @@ export class UIThemeHandler extends LitElement {
 
         switch (name) {
             case "auto":
-                // TODO: Enable/Disable auto mode
+                this.handleAuto();
                 break;
 
             case "mode":
-                // TODO: Set mode
+                this.handleMode();
                 break;
 
             case "theme":
-                // TODO: Set theme
-                break;
-        }
-    }
-}
-
-/**
- * HTML: `ui-theme-handler`
- *
- * Attributes:
- *  - __mode__: *"dark" | "light"*
- *  - __auto__: *boolean*
- */
-export class _UIThemeHandler extends HTMLElement {
-    static register = () => {
-        if (!customElements.get("ui-theme-handler")) {
-            console.debug(`[ui] Register "ui-theme-handler" component`);
-            customElements.define("ui-theme-handler", UIThemeHandler);
-        }
-    };
-
-    static observedAttributes = ["auto", "mode", "theme"];
-
-    constructor() {
-        super();
-
-        /**
-         * @type {((ev: MediaQueryListEvent | MediaQueryList) => void|Promise<void>) | null}
-         */
-        this.mediaHandler = null;
-
-        /**
-         * @type {{ name: string; href: string } | null}
-         */
-        this.currentTheme = null;
-
-        /**
-         * @private
-         * @type {UIThemeHandler_Mode}
-         */
-        this.mode = null;
-
-        /**
-         * @private
-         * @type {UIThemeHandler_Theme}
-         */
-        this.theme = defaultTheme;
-
-        this.ui = {
-            root: this,
-
-            get auto() {
-                return !!this.root.media;
-            },
-
-            set auto(value) {
-                this.root.setAuto(value);
-            },
-
-            get mode() {
-                return this.root.mode;
-            },
-
-            /**
-             * @param {UIThemeHandler_Mode} value
-             */
-            set mode(value) {
-                this.root.setMode(value);
-            },
-
-            get theme() {
-                return this.root.theme;
-            },
-
-            /**
-             * @param {UIThemeHandler_Theme} value
-             */
-            set theme(value) {
-                this.root.theme = value;
-                this.root.setTheme(value || defaultTheme);
-            },
-        };
-    }
-
-    /**
-     * @param {string} name
-     * @param {string | null} _oldValue
-     * @param {string | null} newValue
-     */
-    attributeChangedCallback(name, _oldValue, newValue) {
-        switch (name) {
-            case "auto":
-                this.ui.auto = newValue !== null;
-                break;
-
-            case "mode":
-                // @ts-expect-error
-                this.ui.mode = newValue;
-                break;
-
-            case "theme":
-                // @ts-expect-error
-                this.ui.theme = newValue;
+                this.handleTheme();
                 break;
         }
     }
 
-    /**
-     * @param {boolean} state
-     * @param {HTMLElement} target
-     */
-    setAuto(state, target = document.body) {
+    private handleAuto(): void {
+        const target: HTMLElement = document.body;
+
+        // TODO: Enable/Disable auto mode
         if (!state) {
             if (!this.media) return;
 
@@ -181,11 +78,10 @@ export class _UIThemeHandler extends HTMLElement {
         this.mediaHandler(this.media);
     }
 
-    /**
-     * @param {UIThemeHandler_Mode} value
-     * @param {HTMLElement} target
-     */
-    setMode(value, target = document.body) {
+    private handleMode(): void {
+        const target: HTMLElement = document.body;
+
+        // TODO: Set mode
         this.mode = value;
         if (!this.mode) {
             target.removeAttribute("data-theme");
@@ -194,28 +90,20 @@ export class _UIThemeHandler extends HTMLElement {
         }
     }
 
-    /**
-     * @param {UIThemeHandler_Theme | null} value
-     * @param {object} options
-     * @param {HTMLElement} [options.target]
-     * @param {string} [options.prefixPath]
-     */
-    setTheme(value, options = null) {
-        options = {
-            target: document.head,
-            prefixPath: "/themes",
-            ...(options || {}),
-        };
+    private handleTheme(): void {
+        const target: HTMLElement = document.head;
+        const prefixPath: string = "/themes";
 
-        options.target
-            .querySelectorAll(`link.theme`)
-            .forEach((child) => options.target.removeChild(child));
+        target.querySelectorAll(`link.theme`).forEach((child) => {
+            target.removeChild(child);
+        });
 
         const link = document.createElement("link");
-        link.classList.add("theme");
-        link.rel = "stylesheet";
-        link.href = `${options.prefixPath}/${value || defaultTheme}.css`;
-
-        options.target.appendChild(link);
+        {
+            link.classList.add("theme");
+            link.rel = "stylesheet";
+            link.href = `${prefixPath}/${this.theme || defaultTheme}.css`;
+        }
+        target.appendChild(link);
     }
 }
