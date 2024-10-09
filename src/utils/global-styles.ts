f8581dae027e7ed8a89281d1af70adb5beac5142
Udo Bauer <knackwurstking.tux@gmail.com>
2024-10-09 15:14:22 +0200

    Convert global-styles to ts

---
+++ src/utils/global-styles.ts
@@ -0,0 +1,22 @@
+let globalData: CSSStyleSheet[] | null = null;
+
+export function globalStyleSheets(): CSSStyleSheet[] {
+    if (globalData === null) {
+        globalData = Array.from(document.styleSheets).map((x) => {
+            const sheet = new CSSStyleSheet();
+
+            const css = Array.from(x.cssRules)
+                .map((rule) => rule.cssText)
+                .join(" ");
+
+            sheet.replaceSync(css);
+            return sheet;
+        });
+    }
+
+    return globalData;
+}
+
+export function globalStylesToShadowRoot(shadowRoot: ShadowRoot): void {
+    shadowRoot.adoptedStyleSheets.push(...globalStyleSheets());
+}
