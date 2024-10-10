4ce2040dc742084e71e9f7196e97227c6c7e9d7b
Udo Bauer <knackwurstking.tux@gmail.com>
2024-10-10 17:28:55 +0200

    Set copyPublicDir to false

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
     build: {
         outDir: "dist",
+        copyPublicDir: false,
         lib: {
             entry: "lib/index.ts",
             fileName: "ui.min",
