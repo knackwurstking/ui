8f5a97db1c918971ef23e108c4f0c6994764e117
Udo Bauer <knackwurstking.tux@gmail.com>
2024-10-10 20:44:44 +0200

    Convert ui-primary ts lit

---
+++ lib/ui-text/ui-primary.ts
@@ -0,0 +1,13 @@
+import { html, LitElement } from "lit";
+import { customElement } from "lit/decorators.js";
+
+@customElement("ui-primary")
+export class UIPrimary extends LitElement {
+    render() {
+        return html`
+            <ui-text size="1.1rem" casl="1" mono="0" slnt="0">
+                <slot></slot>
+            </ui-text>
+        `;
+    }
+}
