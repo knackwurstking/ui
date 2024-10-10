3f5a29f782fd7c3388b15b6e18e7311fea9918aa
Udo Bauer <knackwurstking.tux@gmail.com>
2024-10-10 17:12:42 +0200

    Fix imports

--- lib/svg/smoothie-line-icons/pen.ts
+++ lib/svg/smoothie-line-icons/pen.ts
@@ -1,6 +1,4 @@
-import { html } from "../../src";
 import { html } from "../..";
-// TODO: Continue here...
 
 export default html`
     <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
--- lib/svg/smoothie-line-icons/power.ts
+++ lib/svg/smoothie-line-icons/power.ts
@@ -1,4 +1,4 @@
-import { html } from "../../src";
+import { html } from "../..";
 
 export default html`
     <svg
