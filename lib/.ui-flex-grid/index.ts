25a90bd0e7af0f3cab30c10d0dcbd9e52a8cf8bb
Udo Bauer <knackwurstking.tux@gmail.com>
2024-10-11 19:29:38 +0200

    Convert ui-flex-grid-item to typescript lit component

--- lib/.ui-flex-grid/index.ts
+++
@@ -1,4 +0,0 @@
-// TODO: Convert to typescript
-export { UIFlexGridItem } from "./ui-flex-grid-item";
-export { UIFlexGridRow } from "./ui-flex-grid-row";
-export { UIFlexGrid } from "./ui-flex-grid";
---
+++ lib/ui-flex-grid/ui-flex-grid-item.ts
@@ -0,0 +1,44 @@
+import { css, html, LitElement } from "lit";
+import { customElement, property } from "lit/decorators.js";
+
+const defaultFlex = 1;
+
+@customElement("ui-flex-grid-item")
+export class UIFlexGridItem extends LitElement {
+    @property({ type: Number, attribute: "flex" })
+    flex: number = defaultFlex;
+
+    static get styles() {
+        return css`
+            :host {
+                flex: var(--ui-flex-grid-item_flex, ${defaultFlex});
+                display: content;
+            }
+
+            ::slotted(*) {
+                flex-grow: 1;
+            }
+        `;
+    }
+
+    protected render() {
+        return html`<slot></slot>`;
+    }
+
+    attributeChangedCallback(
+        name: string,
+        _old: string | null,
+        value: string | null,
+    ): void {
+        super.attributeChangedCallback(name, _old, value);
+
+        switch (name) {
+            case "flex":
+                this.style.setProperty(
+                    `--ui-flex-grid-item_flex`,
+                    value || defaultFlex.toString(),
+                );
+                break;
+        }
+    }
+}
