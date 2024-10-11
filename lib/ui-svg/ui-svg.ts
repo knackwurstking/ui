dbda55e1478f5141c00ac94932650d6b850d7e3c
Udo Bauer <knackwurstking.tux@gmail.com>
2024-10-11 15:55:25 +0200

    Update ui-svg host styles

--- lib/ui-svg/ui-svg.ts
+++ lib/ui-svg/ui-svg.ts
@@ -1,14 +1,12 @@
 import { css, html, LitElement } from "lit";
+import { customElement } from "lit/decorators.js";
 
+@customElement("ui-svg")
 export class UISvg extends LitElement {
     static get styles() {
         return css`
             :host {
-                display: block;
-
-                width: 100%;
-                height: 100%;
-
+                display: inline-block;
                 color: inherit;
             }
 
