e1ca8a4c3471ac8b61d6b1ef32e00ab619c9b5ee
Udo Bauer <knackwurstking.tux@gmail.com>
2024-10-18 01:07:09 +0200

    Convert ui-drawer-group-item component to lit

---
+++ lib/ui-drawer/index.ts
@@ -0,0 +1,3 @@
+//export { UIDrawer } from "./ui-drawer";
+//export { UIDrawerGroup } from "./ui-drawer-group";
+export { UIDrawerGroupItem } from "./ui-drawer-group-item";
---
+++ lib/ui-drawer/ui-drawer-group-item.ts
@@ -0,0 +1,36 @@
+import { css as CSS, html, LitElement } from "lit";
+import { customElement } from "lit/decorators.js";
+
+@customElement("ui-drawer-group-item")
+export class UIDrawerGroupItem extends LitElement {
+    static get styles() {
+        return CSS`
+            * {
+                box-sizing: border-box;
+            }
+
+            :host {
+                display: block;
+                width: 100%;
+            }
+
+            li {
+                display: flex;
+                flex-direction: row;
+                flex-wrap: nowrap;
+
+                width: 100%;
+
+                padding: var(--ui-spacing) calc(var(--ui-spacing) * 1.5);
+            }
+
+            ::slotted(*) {
+                width: 100%;
+            }
+        `;
+    }
+
+    protected render(): unknown {
+        return html`<li><slot></slot></li>`;
+    }
+}
