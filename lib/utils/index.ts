aa041759a4a7ce20ff42b2ffc1426a6bc4a58644
Udo Bauer <knackwurstking.tux@gmail.com>
2024-12-09 00:22:55 +0100

    Export `String.raw` instead of lit html and css

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -1,10 +1,12 @@
 export * from "./global-styles";
 export * from "./events";
 export * from "./cleanup";
-export { html, css } from "lit";
 
 export * as draggable from "./draggable";
 export * as ripple from "./ripple";
+
+export const html = String.raw;
+export const css = String.raw;
 
 export function isAndroid(): boolean {
     return /(android)/i.test(navigator.userAgent);
@@ -13,10 +15,7 @@ export function isAndroid(): boolean {
 export function styles(style: CSSStyleDeclaration): string {
     return (
         Object.entries(style)
-            .map(
-                ([k, v]) =>
-                    `${k.replace(/[A-Z]/g, (l) => `-${l.toLowerCase()}`)}:${v}`,
-            )
+            .map(([k, v]) => `${k.replace(/[A-Z]/g, (l) => `-${l.toLowerCase()}`)}:${v}`)
             .join(";") + ";"
     );
 }
