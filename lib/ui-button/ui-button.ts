c4957959706d5432ef7f3b0a2dfca1fe59be458a
Udo Bauer <knackwurstking.tux@gmail.com>
2024-12-08 23:51:46 +0100

    Remove ui-button (js) component styles

--- lib/ui-button/ui-button.ts
+++ lib/ui-button/ui-button.ts
@@ -1,4 +1,4 @@
-import { css, html, LitElement } from "lit";
+import { html, LitElement } from "lit";
 import { customElement, property } from "lit/decorators.js";
 import { CleanUpFunction } from "../global";
 import { ripple } from "../utils";
@@ -28,76 +28,6 @@ class UIButton extends LitElement {
     role: string | null = "button";
 
     private rippleCleanUp: CleanUpFunction | null = null;
-
-    static get styles() {
-        return css`
-            :host {
-                border: 1px solid currentColor;
-            }
-
-            :host([variant="full"]) {
-                border: none;
-            }
-
-            :host([variant="full"][color="primary"]) {
-                background-color: var(--ui-primary);
-                color: var(--ui-primary-text);
-            }
-
-            :host([variant="full"][color="secondary"]) {
-                background-color: var(--ui-secondary);
-                color: var(--ui-secondary-text);
-            }
-
-            :host([variant="full"][color="destructive"]) {
-                background-color: var(--ui-destructive);
-                color: var(--ui-destructive-text);
-            }
-
-            :host([variant="outline"]) {
-                border-color: currentColor;
-                background-color: transparent;
-            }
-
-            :host([variant="outline"][color="primary"]) {
-                color: var(--ui-primary);
-            }
-
-            :host([variant="outline"][color="secondary"]) {
-                color: var(--ui-secondary);
-            }
-
-            :host([variant="outline"][color="destructive"]) {
-                color: var(--ui-destructive);
-            }
-
-            :host([variant="ghost"]) {
-                border-color: transparent;
-                background-color: transparent;
-            }
-
-            :host([variant="ghost"][color="primary"]) {
-                color: var(--ui-primary);
-            }
-
-            :host([variant="ghost"][color="secondary"]) {
-                color: var(--ui-secondary);
-            }
-
-            :host([variant="ghost"][color="destructive"]) {
-                color: var(--ui-destructive);
-            }
-
-            :host([disabled]),
-            :host([disabled]:hover),
-            :host([disabled]:active) {
-                background-color: transparent;
-                opacity: 0.25;
-                cursor: default;
-                pointer-events: none;
-            }
-        `;
-    }
 
     protected render() {
         return html`<slot></slot>`;
