ea15a6efc66a5cfc08ddd0ab021b4fc8b4d9a0db
Udo Bauer <knackwurstking.tux@gmail.com>
2024-12-24 07:49:09 +0100

    Location handler on "haschange" event

--- public/script.js
+++ public/script.js
@@ -7,6 +7,17 @@ const routes = {
     "04": {},
 };
 
+function goto(route) {
+    // TODO: Fetch and write content to document using open/write/close
+}
+
 window.addEventListener("hashchange", () => {
-    // TODO: Location handler
+    const location = window.location.hash.replace("#", "");
+
+    let route = routes[location] || null;
+    if (route == null) {
+        route = routes[404];
+    }
+
+    goto(route);
 });
