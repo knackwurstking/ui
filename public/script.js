0cd1119ceff3562a3fcf8021b58eead8a8df08ca
Udo Bauer <knackwurstking.tux@gmail.com>
2024-12-24 07:38:03 +0100

    @TODO: routing

---
+++ public/script.js
@@ -0,0 +1,12 @@
+const routes = {
+    404: {},
+    "/": {},
+    "01": {},
+    "02": {},
+    "03": {},
+    "04": {},
+};
+
+window.addEventListener("hashchange", () => {
+    // TODO: Location handler
+});
