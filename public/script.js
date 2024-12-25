714c99e9137041bfeedca84916428edc63d46d59
Udo Bauer <knackwurstking.tux@gmail.com>
2024-12-25 10:32:20 +0100

    Add routing

--- public/script.js
+++ public/script.js
@@ -1,23 +1,39 @@
 const routes = {
-    404: {},
-    "/": {},
-    "01": {},
-    "02": {},
-    "03": {},
-    "04": {},
+    "/": {
+        title: "UI | Index",
+        path: "/",
+    },
+    "01": {
+        title: "UI | 01 Test",
+        path: "/01-test.html",
+    },
+    "02": {
+        title: "UI | HTML5 Test Page",
+        path: "/02-html5-test-page.html",
+    },
+    "03": {
+        title: "UI | SVGs",
+        path: "/03-svgs.html",
+    },
+    "04": {
+        title: "UI | Mobile Layout",
+        path: "/04-mobile-layout.html",
+    },
 };
 
 function goto(route) {
-    // TODO: Fetch and write content to document using open/write/close
+    fetch(route.path)
+        .then((r) => r.text())
+        .then((d) => {
+            document.querySelector(`head > title`).innerText = route.title;
+            document.body.innerHTML = d;
+        })
+        .catch((err) => alert(err));
 }
 
 window.addEventListener("hashchange", () => {
-    const location = window.location.hash.replace("#", "");
-
-    let route = routes[location] || null;
-    if (route == null) {
-        route = routes[404];
-    }
-
-    goto(route);
+    goto(routes[window.location.hash.replace("#", "")] || routes["/"]);
 });
+
+// Call haschange handler once
+window.dispatchEvent(new Event("hashchange"));
