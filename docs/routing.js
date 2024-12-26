83dfe56f360b4172da36ae5807cbd5b5f5a25dee
Udo Bauer <knackwurstking.tux@gmail.com>
2024-12-26 14:04:38 +0100

    Use relative paths

--- docs/routing.js
+++ docs/routing.js
@@ -2,28 +2,28 @@ window.addEventListener("DOMContentLoaded", () => {
     const routes = {
         "/": {
             title: "UI | Index",
-            template: "/",
+            template: "",
             scripts: [],
         },
         "01": {
             title: "UI | 01 Test",
-            template: "/01-test.template.html",
+            template: "01-test.template.html",
             scripts: [],
         },
         "02": {
             title: "UI | HTML5 Test Page",
-            template: "/02-html5-test-page.template.html",
+            template: "02-html5-test-page.template.html",
             scripts: [],
         },
         "03": {
             title: "UI | SVGs",
-            template: "/03-svgs.template.html",
+            template: "03-svgs.template.html",
             scripts: [],
         },
         "04": {
             title: "UI | Mobile Layout",
-            template: "/04-mobile-layout.template.html",
-            scripts: [{ src: "/04-mobile-layout.js" }],
+            template: "04-mobile-layout.template.html",
+            scripts: [{ src: "04-mobile-layout.js" }],
         },
     };
 
