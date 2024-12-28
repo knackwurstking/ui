ea747dcc8fddb132a85a788f193cfa9785a24ba3
Udo Bauer <knackwurstking.tux@gmail.com>
2024-12-28 15:53:08 +0100

    Just use the hash export for the router package

--- lib/utils/router/hash-router.ts
+++ lib/utils/router/hash-router.ts
@@ -1,48 +1,6 @@
 import { Route } from "./types";
 
-export function router(mode: "url" | "hash") {
-    switch (mode) {
-        case "url":
-            throw `the url mode is under construction, use hash`;
-
-        case "hash":
-            hashRouter();
-            break;
-
-        default:
-            throw `unknown mode: ${mode}`;
-    }
-}
-
-export function hashRouter() {
-    const routes: { [key: string]: Route } = {
-        "/": {
-            title: "UI | Index",
-            template: "",
-            scripts: [],
-        },
-        "01": {
-            title: "UI | 01 Test",
-            template: "01-test.template.html",
-            scripts: [],
-        },
-        "02": {
-            title: "UI | HTML5 Test Page",
-            template: "02-html5-test-page.template.html",
-            scripts: [],
-        },
-        "03": {
-            title: "UI | SVGs",
-            template: "03-svgs.template.html",
-            scripts: [],
-        },
-        "04": {
-            title: "UI | Mobile Layout",
-            template: "04-mobile-layout.template.html",
-            scripts: [{ src: "04-mobile-layout.js" }],
-        },
-    };
-
+export function hash(routes: { [key: string]: Route }) {
     function goto(route: Route) {
         fetch(route.template)
             .then((r) => r.text())
