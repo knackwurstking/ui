a1426e990d5d86c8ae0cf3045c45c01240992e3d
Udo Bauer <knackwurstking.tux@gmail.com>
2025-04-01 11:37:57 +0200

    feat: simplify router type

--- lib/utils/router/types.ts
+++ lib/utils/router/types.ts
@@ -1,18 +1,5 @@
 export interface Route {
     title?: string;
-    href?: string;
-    scripts?: Script[];
     onMount?: () => void | Promise<void>;
     onDestroy?: () => void | Promise<void>;
-
-    template?: {
-        target?: string;
-        selector?: string;
-        onMount?: () => void | Promise<void>;
-        onDestroy?: () => void | Promise<void>;
-    };
-}
-
-export interface Script {
-    src: string;
 }
