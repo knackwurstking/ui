e15e81d269cfd88e44a029c31024c743909bcee9
Udo Bauer <knackwurstking.tux@gmail.com>
2025-04-01 11:40:57 +0200

    feat: simplify hash router

--- lib/utils/router/hash-router.ts
+++ lib/utils/router/hash-router.ts
@@ -1,92 +1,21 @@
 import { Route } from "./types";
 
-export function init(target: Element | null, routes: { [key: string]: Route }): void {
-    //let pushedState = false;
+export function init(routes: { [key: string]: Route }): void {
     let current: Route | null = null;
-
     async function goto(route: Route) {
-        //if (location.hash.match(/.*\?+/)) {
-        //    const [hash, search] = location.hash.split("?", 2);
-        //    const url = location.origin + location.pathname + `?${search}` + hash;
-        //    history.pushState({ path: url, modified: true }, "", url);
-        //    pushedState = true;
-        //} else {
-        //    pushedState = false;
-        //}
-
         if (current !== null) {
-            if (current.template?.onDestroy !== undefined) {
-                current.template.onDestroy();
-            }
-
-            if (current.onDestroy !== undefined) {
-                current.onDestroy();
-            }
+            if (!!current.onDestroy) current.onDestroy();
         }
 
         current = route;
 
         if (current.title !== undefined) {
             const title = document.querySelector(`head > title`);
-            if (title !== null) {
-                title.innerHTML = current.title;
-            }
+            if (!!title) title.innerHTML = current.title;
         }
 
-        if (current?.href !== undefined && !!target) {
-            const resp = await fetch(current.href);
-            const data = await resp.text();
-
-            target.innerHTML = data;
-
-            if (current.scripts !== undefined) {
-                current.scripts.forEach((s) => {
-                    const script = document.createElement("script");
-                    script.setAttribute("data-template", current!.href!);
-                    script.src = s.src;
-                    target.appendChild(script);
-                });
-            }
-        }
-
-        if (current?.onMount !== undefined) {
-            current.onMount();
-        }
-
-        if (!!current?.template) {
-            let templateTarget = target;
-            if (current.template.target !== undefined) {
-                templateTarget = document.querySelector(current.template.target)!;
-            }
-
-            if (!!current.template.selector && !!templateTarget) {
-                const template = document.querySelector(current.template.selector);
-                if (!template) {
-                    throw `${current.template.selector} not found`;
-                }
-
-                if (template instanceof HTMLTemplateElement) {
-                    templateTarget.innerHTML = "";
-                    templateTarget.appendChild(template.content.cloneNode(true));
-                } else {
-                    templateTarget.innerHTML = template.innerHTML;
-                }
-            }
-
-            if (current.template?.onMount !== undefined) {
-                current.template.onMount();
-            }
-        }
+        if (!!current.onMount) current.onMount();
     }
-
-    //window.addEventListener("popstate", () => {
-    //    if (!pushedState) {
-    //        return;
-    //    }
-
-    //    history.back();
-    //    pushedState = false;
-    //});
 
     window.addEventListener("hashchange", () => {
         const hash = window.location.hash.replace("#", "");
@@ -120,13 +49,18 @@ export function init(target: Element | null, routes: { [key: string]: Route }): 
     window.dispatchEvent(new Event("hashchange"));
 }
 
-export function goTo(query: { [key: string]: string } | null, hash: string): void {
+export function goTo(
+    query: { [key: string]: string } | null,
+    hash: string,
+): void {
     let search: string;
     if (!query) {
         search = "";
     } else {
         search = `?${Object.entries(query)
-            .map(([k, v]) => `${encodeURIComponent(k)}=${encodeURIComponent(v)}`)
+            .map(
+                ([k, v]) => `${encodeURIComponent(k)}=${encodeURIComponent(v)}`,
+            )
             .join("&")}`;
     }
 
@@ -135,7 +69,8 @@ export function goTo(query: { [key: string]: string } | null, hash: string): voi
         return;
     }
 
-    location.hash = `#${encodeURIComponent(hash)}` + (!!search ? `&${search}` : "");
+    location.hash =
+        `#${encodeURIComponent(hash)}` + (!!search ? `&${search}` : "");
 }
 
 export function getSearchParam(): { [key: string]: string } {
