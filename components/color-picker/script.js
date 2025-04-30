5a38b4fefb680d202eb1577ece70feed0c602c01
Udo Bauer <knackwurstking.tux@gmail.com>
2025-04-30 14:51:02 +0200

    wip: query all elements

--- components/color-picker/script.js
+++ components/color-picker/script.js
@@ -1,1 +1,17 @@
+// TODO: First query all elements...
+//  - Need to allow multiple touch inputs (or should i use pointerevents?)
 
+// These are the elements for the input handlers (x-axis)
+const redCircle = document.querySelector(`.color-picker .red .circle`);
+const greenCircle = document.querySelector(`.color-picker .green .circle`);
+const blueCircle = document.querySelector(`.color-picker .blue .circle`);
+
+// When handling the pointer events, this container are the limits for moving the circles
+const rangeContainer = document.querySelector(
+    `.color-picker .container .range`,
+);
+
+// Manuel (number) inputs
+const redInput = document.querySelector(`.color-picker .red input`);
+const greenInput = document.querySelector(`.color-picker .green input`);
+const blueInput = document.querySelector(`.color-picker .blue input`);
