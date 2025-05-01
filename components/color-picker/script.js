35990ba535096d499a2fc01367915a161b96f220
Udo Bauer <knackwurstking.tux@gmail.com>
2025-05-01 06:24:05 +0200

    wip: got a bit of calculations done for moving the circles

--- components/color-picker/script.js
+++ components/color-picker/script.js
@@ -52,30 +52,77 @@ const xCircleMargins = [
 
 // Circles
 
-circles.forEach((circle) => {
-    circle.addEventListener("pointerdown", startMove);
-    window.addEventListener("pointermove", move);
-    window.addEventListener("pointerup", endMove);
+circles.forEach((circle, circleIndex) => {
+    let movementlock = true;
+
+    /**
+     * @param {PointerEvent} ev
+     */
+    function pointerStart(ev) {
+        if (!movementlock) return;
+        ev.preventDefault();
+        movementlock = false;
+    }
+
+    /**
+     * @param {PointerEvent} ev
+     */
+    function pointerEnd(ev) {
+        if (!movementlock) return;
+        ev.preventDefault();
+        movementlock = true;
+    }
+
+    /**
+     * @param {PointerEvent} ev
+     */
+    function pointerMove(ev) {
+        if (movementlock) return;
+        move(circleIndex, ev.clientX, ev.clientY);
+    }
+
+    circle.addEventListener("pointerdown", pointerStart);
+    window.addEventListener("pointermove", pointerMove);
+    window.addEventListener("pointerup", pointerEnd);
 });
 
 /**
- * @param {PointerEvent} ev
+ * @param {number} circleIndex
+ * @param {number} x
+ * @param {number} y
+ * @returns {void}
  */
-function startMove(ev) {
-    ev.preventDefault();
-}
+function move(circleIndex, x) {
+    /** Range Rect */
+    const rR = rectRangeContainers[circleIndex];
+    /** Circle Rect */
+    const cR = rectCircles[circleIndex];
+    /** x (Circle) Margin */
+    const xM = xCircleMargins[circleIndex];
 
-/**
- * @param {PointerEvent} ev
- */
-function move(ev) {
-    // TODO: Move the circle around (x-axis)
-}
+    const xLeft = rR.left + xM;
+    const xRight = rR.right + xM;
 
-/**
- * @param {PointerEvent} ev
- */
-function endMove(ev) {
-    ev.preventDefault();
-    // ...
+    if (cR.left <= xLeft || cR.right >= xRight) {
+        // There is nothing to move anymore, you hit the limits
+        return;
+    }
+
+    const cRadius = (cR.right - cR.left) / 2;
+    const circle = circles[circleIndex];
+
+    if (x - cRadius <= xLeft) {
+        // Just move to the limit and return (.05rem === x-margin)
+        circle.style.right = `calc(100% - 0.05rem)`;
+        circle.style.transform = "";
+        return;
+    }
+    if (x + cRadius >= xRight) {
+        // Just move to the limit and return (.05rem === x-margin)
+        circle.style.right = `calc(0.05rem)`;
+        circle.style.transform = `translateX(+100%)`;
+        return;
+    }
+
+    circle.style.right = `${x + cR.right - cR.left + cRadius}px`; // TODO: Calc percentage here
 }
