4ef25b82e8ea71a377491dd62e319744233f6800
Udo Bauer <knackwurstking.tux@gmail.com>
2025-05-20 00:50:21 +0200

    close socket on error and some clean up

--- lib/utils/ws/index.ts
+++ lib/utils/ws/index.ts
@@ -2,51 +2,23 @@ import { Events } from "../events";
 
 export class WS<D = any> {
     public url: string = "";
-
     public socket: WebSocket | null = null;
     public events = new Events<{
         open: WS;
         close: WS;
+        error: WS;
         message: D;
     }>();
-
     public reconnectInterval = 1000;
 
     protected timeout: NodeJS.Timeout | null = null;
     protected open = false;
 
-    protected onClose = () => {
-        if (this.timeout !== null) {
-            clearTimeout(this.timeout);
-            this.timeout = null;
-        }
-
-        if (this.open) {
-            this.open = false;
-            this.events.dispatch("close", this);
-        }
-
-        // Reconnect here
-        this.timeout = setTimeout(async () => {
-            await this.connect();
-        }, this.reconnectInterval);
-    };
-
-    protected onOpen = () => {
-        if (!this.open) {
-            this.open = true;
-            this.events.dispatch("open", this);
-        }
-    };
-
-    protected onMessage = async (ev: MessageEvent<Blob>) => {
-        const data: D = JSON.parse(await ev.data.text());
-        this.events.dispatch("message", data);
-    };
-
     public constructor(url: string) {
         this.url = url;
     }
+
+    protected onClose = () => {};
 
     public isOpen() {
         return this.open;
@@ -58,9 +30,48 @@ export class WS<D = any> {
         this.socket = new WebSocket(this.url);
 
         // Reconnect handler
-        this.socket.addEventListener("close", this.onClose);
-        this.socket.addEventListener("open", this.onOpen);
-        this.socket.addEventListener("message", this.onMessage);
+        this.socket.onopen = () => {
+            this.open = true;
+            this.events.dispatch("open", this);
+
+            this.socket!.onclose = () => {
+                if (this.timeout !== null) {
+                    clearTimeout(this.timeout);
+                    this.timeout = null;
+                }
+
+                if (this.open) {
+                    this.open = false;
+                    this.events.dispatch("close", this);
+                }
+
+                // Reconnect here
+                this.timeout = setTimeout(
+                    async () => await this.connect(),
+                    this.reconnectInterval,
+                );
+            };
+
+            this.socket!.onmessage = async (ev) => {
+                if (!(ev.data instanceof Blob)) {
+                    console.error(
+                        "Expecting message data from type Blob",
+                        ev.data,
+                    );
+                    return;
+                }
+
+                this.events.dispatch(
+                    "message",
+                    JSON.parse(await ev.data.text()),
+                );
+            };
+        };
+
+        this.socket.addEventListener("error", () => {
+            if (!this.socket) return;
+            this.socket.close();
+        });
     }
 
     public close() {
@@ -71,8 +82,12 @@ export class WS<D = any> {
 
         if (this.socket) {
             this.socket.removeEventListener("close", this.onClose);
-            if (this.isOpen()) this.socket.close();
-            this.socket = null;
+
+            try {
+                this.socket.close();
+            } finally {
+                this.socket = null;
+            }
         }
     }
 }
