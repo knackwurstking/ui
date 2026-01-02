3bbc81c71b491aba64d7972d23bc08f3248131aa
Udo Bauer <knackwurstking.tux@gmail.com>
2026-01-02 09:26:22 +0100

    fix(ui): remove unused HtmxOptions struct and its Attributes method

--- ui-templ/options.go
+++ ui-templ/options.go
@@ -1,65 +1,8 @@
 package ui
 
 import (
-	"fmt"
-
 	"github.com/a-h/templ"
 )
-
-type HtmxOptions struct {
-	Method          string
-	Href            templ.SafeURL
-	Trigger         string
-	Swap            string
-	SwapOOB         string
-	Preserve        string
-	OnResponseError string
-	OnBeforeRequest string
-	OnAfterRequest  string
-}
-
-func (o *HtmxOptions) Attributes() templ.Attributes {
-	a := templ.Attributes{}
-
-	if o.Href != "" {
-		method := "get"
-		if o.Method != "" {
-			method = o.Method
-		}
-		method = fmt.Sprintf("hx-%s", o.Method)
-		a[method] = string(o.Href)
-	}
-
-	if o.Trigger != "" {
-		a["hx-trigger"] = o.Trigger
-	}
-
-	if o.Swap != "" {
-		a["hx-swap"] = o.Swap
-	}
-
-	if o.SwapOOB != "" {
-		a["hx-swap-oob"] = o.SwapOOB
-	}
-
-	if o.Preserve != "" {
-		a["hx-preserve"] = o.Preserve
-	}
-
-	if o.OnResponseError != "" {
-		a["hx-on::response-error"] = o.OnResponseError
-	}
-
-	if o.OnBeforeRequest != "" {
-		a["hx-on::before-request"] = o.OnBeforeRequest
-	}
-
-	if o.OnAfterRequest != "" {
-		a["hx-on::after-request"] = o.OnAfterRequest
-	}
-
-	return a
-}
 
 // DialogOptions defines the properties for a dialog component
 type DialogOptions struct {
