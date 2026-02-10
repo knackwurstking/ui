d85cf69685fecda4eb59164596f8d7b06118ed0a
Udo Bauer <knackwurstking.tux@gmail.com>
2026-02-10 10:29:36 +0100

    Add spacing css as golang constants

---
+++ templ/css/spacing.go
@@ -0,0 +1,193 @@
+package css
+
+// Padding constants
+const (
+	P0  = "p-0"
+	P   = "p"
+	PSm = "p-sm"
+	PLg = "p-lg"
+	PXl = "p-xl"
+
+	PX0  = "px-0"
+	PX   = "px"
+	PXSm = "px-sm"
+	PXLg = "px-lg"
+
+	PY0  = "py-0"
+	PY   = "py"
+	PYSm = "py-sm"
+	PYLg = "py-lg"
+
+	PT = "pt"
+	PR = "pr"
+	PB = "pb"
+	PL = "pl"
+)
+
+// Margin constants
+const (
+	M0    = "m-0"
+	M     = "m"
+	MSm   = "m-sm"
+	MLg   = "m-lg"
+	MXl   = "m-xl"
+	MAuto = "m-auto"
+
+	MX0    = "mx-0"
+	MX     = "mx"
+	MXSm   = "mx-sm"
+	MXLg   = "mx-lg"
+	MXAuto = "mx-auto"
+
+	MY0    = "my-0"
+	MY     = "my"
+	MYSm   = "my-sm"
+	MYLg   = "my-lg"
+	MYAuto = "my-auto"
+
+	MT = "mt"
+	MR = "mr"
+	MB = "mb"
+	ML = "ml"
+)
+
+// Width constants
+const (
+	WFull   = "w-full"
+	WAuto   = "w-auto"
+	WFit    = "w-fit"
+	WMin    = "w-min"
+	WMax    = "w-max"
+	WScreen = "w-screen"
+	W0      = "w-0"
+	W25     = "w-25"
+	W50     = "w-50"
+	W75     = "w-75"
+)
+
+// Height constants
+const (
+	HFull   = "h-full"
+	HAuto   = "h-auto"
+	HFit    = "h-fit"
+	HMin    = "h-min"
+	HMax    = "h-max"
+	HScreen = "h-screen"
+	H0      = "h-0"
+	H25     = "h-25"
+	H50     = "h-50"
+	H75     = "h-75"
+)
+
+// Min Width constants
+const (
+	MinW0    = "min-w-0"
+	MinWFull = "min-w-full"
+	MinWFit  = "min-w-fit"
+	MinWMin  = "min-w-min"
+	MinWMax  = "min-w-max"
+)
+
+// Max Width constants
+const (
+	MaxWNone   = "max-w-none"
+	MaxWFull   = "max-w-full"
+	MaxWFit    = "max-w-fit"
+	MaxWMin    = "max-w-min"
+	MaxWMax    = "max-w-max"
+	MaxWScreen = "max-w-screen"
+	MaxWSm     = "max-w-sm"
+	MaxWMd     = "max-w-md"
+	MaxWLg     = "max-w-lg"
+	MaxWXl     = "max-w-xl"
+	MaxW2Xl    = "max-w-2xl"
+	MaxW3Xl    = "max-w-3xl"
+	MaxW4Xl    = "max-w-4xl"
+	MaxW5Xl    = "max-w-5xl"
+)
+
+// Min Height constants
+const (
+	MinH0      = "min-h-0"
+	MinHFull   = "min-h-full"
+	MinHScreen = "min-h-screen"
+	MinHFit    = "min-h-fit"
+	MinHMin    = "min-h-min"
+	MinHMax    = "min-h-max"
+)
+
+// Max Height constants
+const (
+	MaxHNone   = "max-h-none"
+	MaxHFull   = "max-h-full"
+	MaxHScreen = "max-h-screen"
+	MaxHFit    = "max-h-fit"
+	MaxHMin    = "max-h-min"
+	MaxHMax    = "max-h-max"
+)
+
+// Aspect Ratio constants
+const (
+	AspectAuto      = "aspect-auto"
+	AspectSquare    = "aspect-square"
+	AspectVideo     = "aspect-video"
+	AspectPortrait  = "aspect-portrait"
+	AspectLandscape = "aspect-landscape"
+)
+
+// Position constants
+const (
+	Static   = "static"
+	Relative = "relative"
+	Absolute = "absolute"
+	Fixed    = "fixed"
+	Sticky   = "sticky"
+
+	Inset0     = "inset-0"
+	Top0       = "top-0"
+	Right0     = "right-0"
+	Bottom0    = "bottom-0"
+	Left0      = "left-0"
+	TopFull    = "top-full"
+	RightFull  = "right-full"
+	BottomFull = "bottom-full"
+	LeftFull   = "left-full"
+)
+
+// Z-Index constants
+const (
+	Z0    = "z-0"
+	Z10   = "z-10"
+	Z20   = "z-20"
+	Z30   = "z-30"
+	Z40   = "z-40"
+	Z50   = "z-50"
+	ZAuto = "z-auto"
+)
+
+// Display constants
+const (
+	Block       = "block"
+	InlineBlock = "inline-block"
+	Inline      = "inline"
+	Hidden      = "hidden"
+	Grid        = "grid"
+	InlineGrid  = "inline-grid"
+	Contents    = "contents"
+	Table       = "table"
+)
+
+// Visibility constants
+const (
+	Visible   = "visible"
+	Invisible = "invisible"
+)
+
+// Opacity constants
+const (
+	Opacity0   = "opacity-0"
+	Opacity25  = "opacity-25"
+	Opacity50  = "opacity-50"
+	Opacity75  = "opacity-75"
+	Opacity100 = "opacity-100"
+)
