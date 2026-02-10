4799b4113be52b1dbe36a19fb4e6aca4ca3dad3d
Udo Bauer <knackwurstking.tux@gmail.com>
2026-02-10 18:35:12 +0100

    Add spinner const

--- templ/css/component_spinner_templ.go
+++ templ/css/component_spinner_templ.go
@@ -8,6 +8,10 @@ package css
 import "github.com/a-h/templ"
 import templruntime "github.com/a-h/templ/runtime"
 
+const (
+	Spinner = "spinner"
+)
+
 func StylesComponentSpinner() templ.Component {
 	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
