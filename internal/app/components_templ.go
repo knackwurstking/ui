77a260104cf95eb3e17bbd873564b33c55319a20
Udo Bauer <knackwurstking.tux@gmail.com>
2026-02-12 12:22:15 +0100

    Bigger gap for the section content component

--- internal/app/components_templ.go
+++ internal/app/components_templ.go
@@ -86,7 +86,7 @@ func SectionContent() templ.Component {
 			templ_7745c5c3_Var4 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		var templ_7745c5c3_Var5 = []any{css.PLg, css.Flex, css.FlexCol, css.Gap}
+		var templ_7745c5c3_Var5 = []any{css.PLg, css.Flex, css.FlexCol, css.GapXl}
 		templ_7745c5c3_Err = templ.RenderCSSItems(ctx, templ_7745c5c3_Buffer, templ_7745c5c3_Var5...)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
