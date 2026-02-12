7e1855e6bf19d263b82b2907a173bec18dd6e875
Udo Bauer <knackwurstking.tux@gmail.com>
2026-02-12 05:53:10 +0100

    Add debug-all

--- pkg/css/util_debug_templ.go
+++ pkg/css/util_debug_templ.go
@@ -34,7 +34,7 @@ func StylesUtilDebug() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<style>\n\t\t.debug,\n\t\t.debug * {\n\t\t\tborder: var(--ui-border-width) var(--ui-border-style) red;\n\t\t\tborder-radius: var(--ui-radius);\n\t\t}\n\t</style>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<style>\n\t\t.debug {\n\t\t\tborder: var(--ui-border-width) var(--ui-border-style) red;\n\t\t\tborder-radius: var(--ui-radius);\n\t\t}\n\n\t\t.debug-all,\n\t\t.debug-all * {\n\t\t\tborder: var(--ui-border-width) var(--ui-border-style) red;\n\t\t\tborder-radius: var(--ui-radius);\n\t\t}\n\t</style>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
