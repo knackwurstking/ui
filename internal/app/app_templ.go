dcfc85caf7e21bb6ca696345aaefcbd5635c01b9
Udo Bauer <knackwurstking.tux@gmail.com>
2026-02-16 08:01:52 +0100

    Remove TODO

--- internal/app/app_templ.go
+++ internal/app/app_templ.go
@@ -77,7 +77,7 @@ func App() templ.Component {
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
-			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 5, "  ")
+			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 5, " ")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
