e1afa504cc2bf455dc287eb4c42500b1ec137f25
Udo Bauer <knackwurstking.tux@gmail.com>
2026-02-16 07:57:22 +0100

    Add toasts demo

--- internal/app/app_templ.go
+++ internal/app/app_templ.go
@@ -89,6 +89,10 @@ func App() templ.Component {
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
+			templ_7745c5c3_Err = Toasts().Render(ctx, templ_7745c5c3_Buffer)
+			if templ_7745c5c3_Err != nil {
+				return templ_7745c5c3_Err
+			}
 			return nil
 		})
 		templ_7745c5c3_Err = Layout().Render(templ.WithChildren(ctx, templ_7745c5c3_Var2), templ_7745c5c3_Buffer)
